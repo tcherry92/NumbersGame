cba98a471a34aaadbf002ca86ddfaa7a6f5c0e63
THY <eeret@yahoo.com>
2024-08-10 17:54:33 -0700

    Initial Commit

---
+++ NumbersGame/NumbersGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumbersGameApp.swift
+//  NumbersGame
+//
+//  Created by Taylor Cherry on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NumbersGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
